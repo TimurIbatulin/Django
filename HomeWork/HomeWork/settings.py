be79e319ca7847f6b7f25a9a4821fa164ef57a21
Тимур Ибатулин <timuribatulin@iMac-Timur.Dlink>
2024-01-15 15:07:57 +0300

    2

--- HomeWork/HomeWork/settings.py
+++ HomeWork/HomeWork/settings.py
@@ -125,3 +125,28 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': './log/django.log',
+        },
+    },
+    'loggers' : {
+        'django' : {
+            'handlers': ['console', 'file'],
+            'level' : 'INFO',
+        },
+        'HomeWorkapp' : {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
